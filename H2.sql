c03f8bc5dacac0e4218011e6d731dbc6ba6ec95a
Jacques KOZIK <jacques.kzk@gmail.com>
2024-03-28 10:33:31 +0100

    COMMIT: 28/03/2024 base de données

---
+++ H2.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE H2;
+
+USE H2;
+
+CREATE TABLE Immeuble (
+    nom VARCHAR(30),
+    adresse VARCHAR(40) PRIMARY KEY,
+    numero INT,
+    FOREIGN KEY (numero) REFERENCES Syndicat(numero)
+);
+
+CREATE TABLE Appartement (
+    etage INT,
+    numero INT,
+    superficie FLOAT,
+    estLoue BOOLEAN,
+    adresse VARCHAR(40),
+    FOREIGN KEY (adresse) REFERENCES Immeuble(adresse),
+    PRIMARY KEY (numero,adresse)
+);
+
+CREATE TABLE Syndicat(
+    nom VARCHAR(20),
+    adresse VARCHAR(40),
+    numero INT PRIMARY KEY,
+    adresse_mail VARCHAR(30),
+    nom_referent VARCHAR(20)
+);
+
+CREATE TABLE Personne (
+    nom VARCHAR(30),
+    prenom VARCHAR(30),
+    numero INT PRIMARY KEY
+);
+
+CREATE TABLE Occupation(
+    numero INT,
+    statut INT,
+    appartement_numero INT,
+    adresse VARCHAR(40),
+    FOREIGN KEY (numero) REFERENCES Personne(numero),
+    FOREIGN KEY (adresse) REFERENCES Appartement(adresse),
+    FOREIGN KEY (appartement_numero) REFERENCES Appartement(numero)
+);
