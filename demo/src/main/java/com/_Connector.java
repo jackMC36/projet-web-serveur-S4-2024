631ddfbbd4f0d79db3b7bbb16e8fcd16dff8fb6f
Jacques KOZIK <jacques.kzk@gmail.com>
2024-04-04 10:59:46 +0200

    COMMIT: 03/04/2024

---
+++ demo/src/main/java/com/_Connector.java
@@ -0,0 +1,21 @@
+package main.java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class _Connector {
+    private static Connection connection;
+
+    public static Connection getInstance() {
+        if (connection == null) {
+            try {
+                Class.forName("org.h2.Driver");
+                connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+            } catch (ClassNotFoundException | SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return connection;
+    }
+}
