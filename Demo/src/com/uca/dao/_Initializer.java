616906067b5ab8f728251e763d6bb572944ffcbb
Jacques KOZIK <jacques.kzk@gmail.com>
2024-04-25 19:59:55 +0200

    COMMIT: 25/04/2024

--- Demo/src/com/uca/dao/_Initializer.java
+++ Demo/src/com/uca/dao/_Initializer.java
@@ -10,27 +10,20 @@ public class _Initializer {
 
         try {
 
-            PreparedStatement statementPersonne = connection.prepareStatement("CREATE TABLE Personne ( nom VARCHAR(30), prenom VARCHAR(30), numero INT PRIMARY KEY);");
+            PreparedStatement statementPersonne = connection.prepareStatement("CREATE TABLE IF NOT EXISTS Personne ( nom VARCHAR(30), prenom VARCHAR(30), numero INT PRIMARY KEY);");
             statementPersonne.executeUpdate();
 
-            PreparedStatement statementSyndicat = connection.prepareStatement("CREATE TABLE Syndicat( nom VARCHAR(20), adresse VARCHAR(40), numero INT,  adresse_mail VARCHAR(30), nom_referent VARCHAR(20), PRIMARY KEY (nom, adresse));");
+            PreparedStatement statementSyndicat = connection.prepareStatement("CREATE TABLE IF NOT EXISTS Syndicat( nom VARCHAR(20), adresse VARCHAR(40), numero INT UNIQUE,  adresse_mail VARCHAR(30), nom_referent VARCHAR(20), PRIMARY KEY (nom, adresse));");
             statementSyndicat.executeUpdate();
 
-            PreparedStatement statementImmeuble = connection.prepareStatement("CREATE TABLE Immeuble (nom VARCHAR(30), adresse VARCHAR(40) PRIMARY KEY, numero INT, FOREIGN KEY (numero) REFERENCES Syndicat(numero));");
+            PreparedStatement statementImmeuble = connection.prepareStatement("CREATE TABLE IF NOT EXISTS Immeuble (nom VARCHAR(30), adresse VARCHAR(40) PRIMARY KEY, numero INT, nom_syndicat VARCHAR(20), adresse_syndicat VARCHAR(40), FOREIGN KEY (numero, nom_syndicat, adresse_syndicat) REFERENCES Syndicat(numero, nom, adresse));");
             statementImmeuble.executeUpdate();
 
-            PreparedStatement statementAppartement = connection.prepareStatement("CREATE TABLE Appartement (etage INT, numero INT, superficie FLOAT, estLoue BOOLEAN, adresse VARCHAR(40), FOREIGN KEY (adresse) REFERENCES Immeuble(adresse), PRIMARY KEY (numero,adresse));");
+            PreparedStatement statementAppartement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS Appartement (etage INT, numero INT, superficie FLOAT, estLoue BOOLEAN, adresse VARCHAR(40), FOREIGN KEY (adresse) REFERENCES Immeuble(adresse), PRIMARY KEY (numero,adresse));");
             statementAppartement.executeUpdate();
 
-            PreparedStatement StatementOccupation = connection.prepareStatement("CREATE TABLE Occupation (numero INT, statut INT, appartement_numero INT, adresse VARCHAR(40), FOREIGN KEY (numero) REFERENCES Personne(numero), FOREIGN KEY (adresse) REFERENCES Appartement(adresse), FOREIGN KEY (appartement_numero) REFERENCES Appartement(numero));");
+            PreparedStatement StatementOccupation = connection.prepareStatement("CREATE TABLE IF NOT EXISTS Occupation (numero INT, statut INT, appartement_numero INT, adresse VARCHAR(40), FOREIGN KEY (numero) REFERENCES Personne(numero), FOREIGN KEY (adresse) REFERENCES Appartement(adresse), FOREIGN KEY (appartement_numero) REFERENCES Appartement(numero));");
             StatementOccupation.executeUpdate();
-            
-
-            //Todo Remove me !
-            /*statement = connection.prepareStatement("INSERT INTO users(firstname, lastname) VALUES(?, ?);");
-            statement.setString(1, "Theodore");
-            statement.setString(2, "Muillerez");
-            statement.executeUpdate();*/
 
         } catch (Exception e){
             System.out.println(e.toString());
